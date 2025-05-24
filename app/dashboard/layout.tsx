c5cd61698dcd22db213c5537792d493498ede889
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-24 16:41:18 -0300

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,12 @@
-// app/dashboard/layout.tsx
-'use client'
-import Sidebar from '@/components/Sidebar'
+import Sidebar from './Sidebar'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-black text-white">
+    <div style={{ display: 'flex' }}>
       <Sidebar />
-      <main className="flex-1 p-6">
+      <div style={{ marginLeft: '60px', width: '100%' }}>
         {children}
-      </main>
+      </div>
     </div>
   )
 }
