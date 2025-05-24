2b34a50080531e1a3355e1cd68bd5d1c5cd4a604
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-24 13:39:33 -0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize: '24px' }}>
+      IGOR está vivo
+    </div>
+  )
+}
