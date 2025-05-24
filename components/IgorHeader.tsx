898f8f3e8cf17e20837d4014e25ea99692e9704e
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-24 17:34:08 -0300

    Create IgorHeader.tsx

---
+++ components/IgorHeader.tsx
@@ -0,0 +1,12 @@
+// components/IgorHeader.tsx
+export default function IgorHeader() {
+  return (
+    <div className="w-full flex justify-end p-4">
+      <img
+        src="/sidebar-icons/chat.png"
+        alt="Igor Chat"
+        className="w-10 h-10"
+      />
+    </div>
+  );
+}
