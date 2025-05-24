20f9f608c65b4acdb273f85540fc8180585484b8
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-24 17:58:19 -0300

    Update page.tsx

--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -1,8 +1,7 @@
-// app/igor-chat/page.tsx
 import IgorHeader from "@/components/IgorHeader";
 import IgorChat from "@/components/IgorChat";
 
-export default function IgorChatPage() {
+export default function Page() {
   return (
     <div className="flex flex-col h-screen bg-black text-white">
       <IgorHeader />
