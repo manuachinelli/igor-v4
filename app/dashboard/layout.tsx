e1cfe61ef815916e2d39d8ae3c9c87fab011419a
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-24 15:57:44 -0300

    Create layout.tsx

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+// app/dashboard/layout.tsx
+import Sidebar from '@/components/Sidebar'
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen bg-black text-white">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        {children}
+      </main>
+    </div>
+  )
+}
