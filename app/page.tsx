ab6433e2db916fd4dc618a8f707b8b7f23f49bf8
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-24 13:53:41 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import LandingLoader from '../components/LandingLoader'
+
 export default function HomePage() {
-  return (
-    <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize: '24px' }}>
-      IGOR está vivo
-    </div>
-  )
+  return <LandingLoader />
 }
