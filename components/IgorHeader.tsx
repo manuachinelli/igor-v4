ef93a35490a79b1acb083683b3972314a4793ccc
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-25 20:22:11 -0300

    Update IgorHeader.tsx

--- components/IgorHeader.tsx
+++ components/IgorHeader.tsx
@@ -1,13 +1,3 @@
-// Ajuste menor para forzar deploy
-
 export default function IgorHeader() {
-  return (
-    <div className="w-full flex justify-end p-4">
-      <img
-        src="/sidebar-icons/chat.png"
-        alt="Igor Chat"
-        className="w-8 h-8 opacity-80"
-      />
-    </div>
-  );
+  return null;
 }
