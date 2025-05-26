d228a6f1b9c59b1692ff784840615c588669953a
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-25 22:07:51 -0300

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,17 @@
+'use client';
+
 import Sidebar from './Sidebar';
 import ChatHistoryBar from '@/components/ChatHistoryBar';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const handleNewChat = () => {
+    window.location.reload();
+  };
+
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
       <Sidebar />
-      <ChatHistoryBar />
+      <ChatHistoryBar onNewChat={handleNewChat} />
       <div style={{ flexGrow: 1 }}>
         {children}
       </div>
