b9fe2b47866385c8d6065aa959a90b30b43b80f6
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-29 15:12:22 -0300

    Update middleware-disabled.ts

--- middleware-disabled.ts
+++ middleware-disabled.ts
@@ -1,22 +1,20 @@
-// import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
-// import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 
-// export async function middleware(req: NextRequest) {
-//   const res = NextResponse.next()
-//   const supabase = createMiddlewareClient({ req, res })
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
 
-//   const {
-//     data: { session },
-//   } = await supabase.auth.getSession()
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
-//   if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
-//     return NextResponse.redirect(new URL('/', req.url))
-//   }
+  // Redirigir a login si NO hay sesión y entra a /dashboard
+  if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
 
-//   return res
-// }
-
-// export const config = {
-//   matcher: ['/dashboard/:path*'],
-// }
+  return res
+}
