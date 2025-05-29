072fcbee4f0279cb8ee92c99257264829b270842
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-29 15:14:33 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@
 const nextConfig = {
   reactStrictMode: true
 }
+
 module.exports = nextConfig
+
+// Esto le dice a Next que use el middleware para todo lo que esté bajo /dashboard
+module.exports.middleware = {
+  matcher: ['/dashboard/:path*'],
+}
