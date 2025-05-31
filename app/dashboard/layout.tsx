9b1fc7b3ba5d9287384a2f575430367d0a6f0952
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-31 14:35:34 -0300

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,27 +1,22 @@
 // app/layout.tsx
-import '../globals.css';
-import { Inter } from 'next/font/google';
+import '../globals.css'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" className={inter.className}>
-      <body
-        style={{
-          margin: 0,
-          backgroundColor: '#000',
-          color: '#fff',
-          fontFamily: 'inherit',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          minHeight: '100vh',
-        }}
-      >
+      <body style={{
+        margin: 0,
+        backgroundColor: '#000',
+        color: '#fff',
+        fontFamily: 'inherit',
+        display: 'block',
+        minHeight: '100vh'
+      }}>
         {children}
       </body>
     </html>
-  );
+  )
 }
-
