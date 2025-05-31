24779bed629aa6e667a492b0c88e3e18c329723b
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-31 15:11:28 -0300

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,9 @@ import Sidebar from '@/components/Sidebar';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div style={{ display: 'flex', height: '100%' }}>
+    // Este div “rompe” el centrado global con width:100vw,
+    // obligando a que el Dashboard ocupe todo el ancho.
+    <div style={{ width: '100vw', display: 'flex', height: '100vh' }}>
       <Sidebar />
       <div style={{ flex: 1 }}>
         {children}
