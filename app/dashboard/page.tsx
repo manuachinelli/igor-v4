0e0dfb4783cc1da56c11e6de346c7009f4717cf8
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-06-01 14:51:40 -0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,7 @@
 'use client'
 
-import './page.css'
+import IgorBubbles from '@/components/IgorBubbles'
 
 export default function DashboardPage() {
-  return (
-    <div className="dashboard-container">
-      <div className="metrics">
-        <div className="metric">
-          <div className="circle">35</div>
-          <span>Procesos</span>
-        </div>
-        <div className="metric">
-          <div className="circle">156</div>
-          <span>Ejecuciones 24hs</span>
-        </div>
-        <div className="metric">
-          <div className="circle">156</div>
-          <span>Ejecuciones mes</span>
-        </div>
-        <div className="metric">
-          <div className="circle error">3</div>
-          <span>Errores</span>
-        </div>
-      </div>
-
-      <div className="task-bar">
-        <p>Tareas usadas: 22 / 50</p>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: '44%' }}></div>
-        </div>
-      </div>
-    </div>
-  )
+  return <IgorBubbles />
 }
