4c618120e569d2106562efcc59a9ed31f0fbb274
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-06-03 18:02:58 -0300

    Update page.tsx

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,7 +1,12 @@
-'use client'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+const supabase = createBrowserSupabaseClient()
 
-import IgorOnboardingChat from '@/components/onboarding/IgorOnboardingChat'
-
-export default function OnboardingPage() {
-  return <IgorOnboardingChat />
-}
+useEffect(() => {
+  const setUserId = async () => {
+    const { data } = await supabase.auth.getUser()
+    if (data?.user?.id) {
+      localStorage.setItem('igor-user-id', data.user.id)
+    }
+  }
+  setUserId()
+}, [])
