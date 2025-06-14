e644d3059516bf01275ef6023a3e69d54565dbd8
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-06-13 21:18:14 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
+export const metadata = {
+  title: 'IGORS AI',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
