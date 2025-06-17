fc9a4eccbe33319339f7cb12e8b8b3a6d1c9b8dc
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-06-17 20:24:54 -0300

    Create page.tsx

---
+++ app/dashboard/billing/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function BillingPage() {
+  return (
+    <div>
+      <h1>Billing</h1>
+      <p>Aquí va la información de facturación del usuario.</p>
+    </div>
+  )
+}
